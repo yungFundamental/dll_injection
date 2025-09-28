29fd2629d7ac03b66b581f9c1547a17cfb5377b0
yungFundamental <amit.barzilai22@gmail.com>
2025-09-29 00:17:17 +0300

    Added more specific test

--- injected/library.cpp
+++ injected/library.cpp
@@ -8,7 +8,7 @@ void hijackMessages() {
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-    std::ofstream("test.txt") << "hello world";
+    std::ofstream("C:\\dawger.txt") << "hello world";
     if (ul_reason_for_call == DLL_PROCESS_ATTACH)
         hijackMessages();
 
