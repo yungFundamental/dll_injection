b126f1c0252605f748d111956ffb01cc670f8750
yungFundamental <amit.barzilai22@gmail.com>
2025-09-28 23:20:30 +0300

    Added PID finder

--- injector/inject.cpp
+++ injector/inject.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <Windows.h>
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -7,6 +8,22 @@ int main(int argc, char *argv[]) {
     }
 
     std::cout << "Starting inject process on \"" << argv[1] << "\" process..." << std::endl;
+
+    const LPSTR windowName = argv[1];
+    HWND windowHandle = FindWindowEx(nullptr, nullptr, windowName, nullptr);
+
+    if (windowHandle == nullptr) {
+        std::cerr << "Failed to find any window." << std::endl;
+        return 1;
+    }
+
+    DWORD processId = 0;
+    if (GetWindowThreadProcessId(windowHandle, &processId) == 0) {
+        std::cerr << "Failed to get process id." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Found process with PID of " << processId << std::endl;
 
     return 0;
 }
