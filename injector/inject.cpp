f66c5fda588a12c3b9c5761e1216970e6df87550
yungFundamental <amit.barzilai22@gmail.com>
2025-09-28 19:39:33 +0300

    Added args

--- injector/inject.cpp
+++ injector/inject.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
-int main() {
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <process_name>" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Starting inject process on \"" << argv[1] << "\" process..." << std::endl;
 
     return 0;
 }
