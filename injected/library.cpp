ce2fb30beb163817919aa22e48815ace97a9621c
yungFundamental <amit.barzilai22@gmail.com>
2025-10-03 00:35:30 +0300

    Added dll example

--- injected/library.cpp
+++ injected/library.cpp
@@ -1,16 +1,30 @@
 #include <windows.h>
-#include <fstream>
 
-using namespace std;
+LONG_PTR prevWindowProc = 0;
 
+LRESULT CALLBACK myWindowProc(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam) {
+    if (uMsg == WM_CHAR) {
+        hwnd = CreateWindowA("STATIC", "Hello", WS_VISIBLE | WS_OVERLAPPEDWINDOW, 100, 100, 500, 100, nullptr,
+                                  nullptr, nullptr, nullptr);
+    }
 
-void hijackMessages() {
+    return CallWindowProcA(reinterpret_cast<WNDPROC>(prevWindowProc), hwnd, uMsg, ' ', lParam);
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-    std::ofstream("C:\\dawger.txt") << "hello world";
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
-        hijackMessages();
+void replaceWindowProc() {
+    HWND npHwnd = FindWindowEx(NULL, NULL, "Notepad", NULL);
+    HWND hEditWnd = GetWindow(npHwnd, GW_CHILD);
+    CreateWindowA("STATIC", "Hello", WS_VISIBLE | WS_OVERLAPPEDWINDOW, 100, 100, 500, 100, NULL, NULL, NULL, NULL);
 
+    prevWindowProc = GetWindowLongPtrA(hEditWnd, GWLP_WNDPROC);
+    SetWindowLongPtrA(hEditWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(myWindowProc));
+}
+
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    CreateWindowA("STATIC", "Hello", WS_VISIBLE | WS_OVERLAPPEDWINDOW, 100, 100, 500, 100, NULL, NULL, NULL, NULL);
+
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+        replaceWindowProc();
     return TRUE;
 }
